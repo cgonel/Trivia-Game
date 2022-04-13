312414d84a9f96d66bc75289a0dfc1e13328752c
Chris Gonel <gonelw@gmail.com>
2022-04-12 20:56:11 -0400

    triviaGame state

---
+++ my-app/src/components/Trivia.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import he from 'he';
+
+export default function Trivia(props){
+    const style = {
+        backgroundColor: "#D6DBF5",
+        border: "none"
+    }
+    const answerElements = props.answers.map(answer => {
+        return ( 
+            <div 
+                className="individualAnswer" 
+                key={answer.id}
+                onClick={() => props.selectAnswer(props.question, answer.id)}
+                style={ answer.isSelected ? style : {}}
+            >
+                {he.decode(answer.answer)}
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            <h2 className="question">{props.question}</h2>
+            <div className="answers">
+                {answerElements}
+            </div>
+            <hr/>
+        </div>
+    )
+}
