07f638418b603c1796eead407fb98685f3efa0a8
Chris Gonel <gonelw@gmail.com>
2022-04-22 15:22:13 -0400

    styled-components

--- my-app/src/components/Trivia.js
+++ my-app/src/components/Trivia.js
@@ -13,64 +13,22 @@ const Answer = styled.div`
     border-radius: 8px;
     cursor: pointer;
     ${props => props.endGame && `opacity: 0.5;`}
-    ${props => props.isSelected &&  `backgroundColor: "#D6DBF5";`}
-    
+    ${props => props.isSelected &&  `background-color: #D6DBF5; border: none;`}
+    ${props => props.correctAnswer &&  `background-color: #94D7A2; border: none; opacity: 1;`}
+    ${props => props.incorrectAnswer &&  `background-color: #F8BCBC; border: none;`}
 `
 
 export default function Trivia(props){
-    const selectedAnswerStyle = {
-        backgroundColor: "#D6DBF5",
-        border: "none"
-    }
-    const correctAnswerStyle = {
-        backgroundColor: "#94D7A2",
-        border: "none",
-        opacity: 1
-    }
-    const incorrectAnswerStyle = {
-        backgroundColor: "#F8BCBC",
-        border: "none"
-    }
-
-    function getAnswerStyle(answer) {
-        if (answer.isCorrectAnswer) {
-            return correctAnswerStyle;
-        }
-        if (answer.selectedIsIncorrect) {
-            return incorrectAnswerStyle;
-        }
-    }
-
     const answerElements = props.answers.map(answer => {
         // style of answers changes based on if game is ended or not
-        // if(props.endGame){ 
-        //     return (
-        //         <Answer
-        //             key={answer.id}
-        //             style={ getAnswerStyle(answer) }
-        //         >
-        //             {he.decode(answer.answer)}
-        //         </Answer>
-        //     )
-        // }else{
-        //     return (
-        //         <Answer 
-        //             key={answer.id}
-        //             onClick={() => props.handleAnswerClick(props.question, answer.id)}
-        //             // isSelected={backgroundColor: {answer.isSelected}}
-        //             style={ answer.isSelected ? selectedAnswerStyle : {}}
-        //         >
-        //             {he.decode(answer.answer)}
-        //         </Answer>
-        //     ) 
-        // }
         return (
             <Answer 
                 key={answer.id}
                 endGame={props.endGame}
-                isSelected={answer.isSelected}
+                isSelected={answer.isSelected} 
+                correctAnswer={props.endGame ? answer.isCorrectAnswer : false}
+                incorrectAnswer={answer.selectedIsIncorrect}
                 onClick={ () => props.handleAnswerClick(props.question, answer.id)}
-                // style={ answer.isSelected ? selectedAnswerStyle : {}}
             >
                 {he.decode(answer.answer)}
             </Answer>
